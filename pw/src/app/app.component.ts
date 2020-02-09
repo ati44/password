20b7fe30b62f2c305a2275fe823b1052f955a4b6
ati44 <altunum@hotmail.com>
2020-02-10 02:03:48 +0300

    first commit

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+
+  length = 0;
+
+  lettersecildimi = false;
+  numbersecildimi = false;
+  symbolsecildimi = false;
+  password = "";
+
+
+  onChangeLength(value: string) {
+    const parsedeger = parseInt(value);
+
+    if (!isNaN(parsedeger)) {
+      this.length = parsedeger;
+    }
+  }
+
+  onLetterSelect() {
+    return this.lettersecildimi = !this.lettersecildimi
+  }
+
+
+
+  butonaTikla() {
+
+    const numbers = "1234567890";
+    const letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    const symbols = "é!'^+%&/()="
+
+    let gecerliDeger = '';
+    if (this.numbersecildimi) {
+
+      gecerliDeger += numbers;
+
+
+    }
+    if (this.lettersecildimi) {
+      gecerliDeger += letters;
+    }
+    if (this.symbolsecildimi) {
+      gecerliDeger += symbols;
+    }
+    let generatePassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = Math.floor(Math.random() * gecerliDeger.length);
+
+      generatePassword += gecerliDeger[element];
+
+      this.password = generatePassword;
+
+    }
+
+
+  }
+
+  numberOn() {
+
+    return this.numbersecildimi = !this.numbersecildimi
+  }
+
+  symbolOn() {
+
+    return this.symbolsecildimi = !this.symbolsecildimi
+  }
+
 }
